564bc6a5f7ee774016516801ccc99eb6333d4092
Manuel Calzolari <manuel.calzolari@gmail.com>
2021-08-26 19:33:55 +0200

    Version 0.3.0

--- shapicant/__init__.py
+++ shapicant/__init__.py
@@ -7,6 +7,6 @@ from ._pandas_selector import PandasSelector
 from ._spark_selector import SparkSelector
 from ._spark_udf_selector import SparkUdfSelector
 
-__version__ = "0.2.1"
+__version__ = "0.3.0"
 
 __all__ = ["BaseSelector", "PandasSelector", "SparkSelector", "SparkUdfSelector"]
